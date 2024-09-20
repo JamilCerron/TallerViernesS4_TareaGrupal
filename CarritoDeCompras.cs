312510839cb683e82eb828ab881cb602bc0b7d68
Huarcaya98 <147445610+Huarcaya98@users.noreply.github.com>
2024-09-20 14:39:24 -0500

    Added Class CarritoDeCompras

--- CarritoDeCompras.cs
+++ CarritoDeCompras.cs
@@ -8,6 +8,56 @@ namespace TareaGrupalPrograS4
 {
     public class CarritoDeCompras
     {
+        private List<Producto> productos;
 
+        public CarritoDeCompras()
+        {
+            productos = new List<Producto>();
+        }
+
+        
+        public void AgregarProductoPorIndice(List<Producto> listaDeProductos, int indice)
+        {
+            if (indice >= 0 && indice < listaDeProductos.Count)
+            {
+                Producto productoSeleccionado = listaDeProductos[indice];
+                productos.Add(productoSeleccionado);
+                Console.WriteLine($"{productoSeleccionado.ObtenerNombre()} ha sido añadido al carrito.\n");
+            }
+            else
+            {
+                Console.WriteLine("Índice inválido.\n");
+            }
+        }
+
+        
+        public float ObtenerPrecioTotal()
+        {
+            float total = 0;
+            foreach (var producto in productos)
+            {
+                total += producto.ObtenerPrecio();
+            }
+            return total;
+        }
+
+        
+        public void MostrarCarrito()
+        {
+            Console.WriteLine("Contenido del carrito:");
+            foreach (var producto in productos)
+            {
+                Console.WriteLine(producto.ObtenerCaracteristicas());
+            }
+            Console.WriteLine($"Total: {ObtenerPrecioTotal()}\n");
+        }
+
+       
+        public void VaciarCarrito()
+        {
+            productos.Clear();
+            Console.WriteLine("El carrito ha sido vaciado.\n");
+        }
     }
+}
 }
